9a5300cd19da6ea43f51d064b4019b631f41f217
mdtech2007 <md.technology.india@gmail.com>
2025-06-27 04:49:19 +0530

    Update ComercialcarApplication.java

--- src/main/java/com/md/car/ComercialcarApplication.java
+++ src/main/java/com/md/car/ComercialcarApplication.java
@@ -2,12 +2,19 @@ package com.md.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ComercialcarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComercialcarApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to my world....";
+	}
 }
