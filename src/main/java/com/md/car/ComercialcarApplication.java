f12fa085b4d6cc2f330ab17ac8b21781b03efa2a
mdtech2007 <md.technology.india@gmail.com>
2025-06-27 03:37:28 +0530

    Add files via upload

---
+++ src/main/java/com/md/car/ComercialcarApplication.java
@@ -0,0 +1,13 @@
+package com.md.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercialcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercialcarApplication.class, args);
+	}
+
+}
